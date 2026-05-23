d9b753ea5bebdec5e68dc0bc5a8d0e5d96f14c9c
sahil dudani <s4sahil.dudani@gmail.com>
2026-05-23 14:43:26 +0530

    hero code addednew

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Navbar from "./components/common-components/navbar";
+// import Navbar from "./components/common-components/navbar";
 import Hero from "./components/landing-page/Hero";
-import Collections from "./components/landing-page/Collections";
-import Features from "./components/landing-page/Features";
-import Newsletter from "./components/landing-page/Newsletter";
-import Footer from "./components/common-components/Footer";
+// import Collections from "./components/landing-page/Collections";
+// import Features from "./components/landing-page/Features";
+// import Newsletter from "./components/landing-page/Newsletter";
+// import Footer from "./components/common-components/Footer";
 
 function App() {
   return (
